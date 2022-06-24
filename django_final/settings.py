ff557e86e9b1c802abe2a2e6a2a51754c2ed82d3
Shigomany <ltropin@mail.ru>
2022-06-25 01:11:49 +0500

    Fix

--- django_final/settings.py
+++ django_final/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_final.wsgi.application'
 
 
 DATABASES = {
-    'default': get_db_config() if not DEBUG else {
+    'default': get_db_config() if DEBUG else {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
